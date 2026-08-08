c92a06c002b8186579b257f6cefebf4a2dc0fc81
princeboghara <pricebprivate@gmail.com>
2026-08-08 15:02:34 +0530

    re

--- middleware/auth.js
+++
@@ -1,9 +0,0 @@
-function checkAdmin(req, res, next) {
-    if (req.session && req.session.isAdmin) {
-        next();
-    } else {
-        res.redirect('/admin/login');
-    }
-}
-
-module.exports = { checkAdmin };
